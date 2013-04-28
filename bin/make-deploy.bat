81eb2d7e100a7711b21c60806d27f2a2253e3101
Dmitry Kann <yktooo@gmail.com>
2013-04-28 11:24:59 +0200

    Fixed file names

--- bin/make-deploy.bat
+++ bin/make-deploy.bat
@@ -4,11 +4,11 @@ if exist procexp.zip del procexp.zip
 if exist procexp-source.zip del procexp-source.zip
 
 rem Archive binary
-%WINRAR% a procexp.zip ..\ProcessExplorer.exe ..\file_id.diz ..\COPYING ..\HISTORY
+%WINRAR% a procexp.zip ..\ProcessExplorer.exe ..\file_id.diz ..\README ..\HISTORY
 if errorlevel == 1 goto err
 
 rem Archive source
-%WINRAR% a procexp-source.zip ..\*.pas ..\*.dpr ..\*.dfm ..\*.dof ..\*.cfg ..\*.res ..\ImageList.bmp ..\file_id.diz ..\COPYING ..\HISTORY
+%WINRAR% a procexp-source.zip ..\*.pas ..\*.dpr ..\*.dfm ..\*.dof ..\*.cfg ..\*.res ..\ImageList.bmp ..\README ..\HISTORY
 if errorlevel == 1 goto err
 goto finish
 
